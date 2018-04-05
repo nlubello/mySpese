7b6e3f9e18e5e3fe4c594fb0ed6bcc4f6dcfb348
Niccolò - Lavoro <nlubello@gmail.com>
2018-04-05 17:17:22 +0200

    Ripristino unique su nome categoria

--- database/migrations/2018_04_04_115402_create_categories_table.php
+++ database/migrations/2018_04_04_115402_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('icon')->nullable();
             $table->integer('type')->default(0);
             $table->timestamps();
