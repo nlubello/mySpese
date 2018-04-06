33b140d9177940211bb13889ae978d7a4ccbd158
Niccolò - Lavoro <nlubello@gmail.com>
2018-04-06 16:22:29 +0200

    feed utente e introiti

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,7 +21,10 @@ class DatabaseSeeder extends Seeder
       App\Models\Category::create(['name' => 'Regali', 'icon' => 'fa-gift', 'type' => 0]);
       App\Models\Category::create(['name' => 'Relazioni', 'icon' => 'fa-heart-o', 'type' => 0]);
       App\Models\Category::create(['name' => 'Altro', 'icon' => 'fa-star-o', 'type' => 0]);
+      App\Models\Category::create(['name' => 'Stipendio', 'icon' => 'fa-money', 'type' => 1]);
+      App\Models\Category::create(['name' => 'Resi', 'icon' => 'fa-building-o', 'type' => 1]);
+      App\Models\Category::create(['name' => 'Fatture', 'icon' => 'fa-paper-plane', 'type' => 1]);
 
-      \Backpack\Base\app\Models\BackpackUser::create(['name' => 'Admin', 'email'=> 'admin@admin.com', 'password' => '123456']);
+      \Backpack\Base\app\Models\BackpackUser::create(['name' => 'Admin', 'email'=> 'admin@admin.com', 'password' => bcrypt('123456')]);
     }
 }
