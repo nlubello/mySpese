f578b0f18faf3fe5b6cd9807e04ca3cc5ca5e1f3
Niccolò Lubello Mac <nlubello@gmail.com>
2020-01-12 20:02:19 +0100

    Fix Vue setup and test

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+                <div class="box box-default">
+                    <div class="box-header">Example Component</div>
 
-                    <div class="card-body">
+                    <div class="box-body">
                         I'm an example component.
                     </div>
                 </div>
--- resources/views/budget/dashboard.blade.php
+++ resources/views/budget/dashboard.blade.php
@@ -14,88 +14,22 @@
 
 
 @section('content')
-  <div class="row">
-    <div class="col-xs-12 text-center">
-      <nav aria-label="Month pagination">
-        @php
-        setlocale(LC_TIME, 'Italian');
-        $dC = Carbon\Carbon::parse($now);
-        @endphp
-
-        <ul class="pagination">
-          @php
-            $d = (clone $dC)->subYear();
-            $dTxt = $d->formatLocalized('%b %Y');
-            $dUrl = $d->startOfMonth()->toDateString();
-          @endphp
-          <li class="page-item hidden-xs"><a class="page-link"
-            href="{{ backpack_url('dashboard') . '?date=' . $dUrl }}">
-            &laquo; {{ $dTxt }}
-          </a></li>
-
-          <li class="page-item disabled hidden-xs"><a class="page-link" href="#">...</a></li>
-
-          @for ($i = 2; $i > 0; $i--)
-            @php
-              $d = (clone $dC)->subMonths($i);
-              $dTxt = $d->formatLocalized('%b %Y');
-              $dUrl = $d->startOfMonth()->toDateString();
-            @endphp
-              <li class="page-item {{$i==2 ? 'hidden-xs' : ''}}"><a class="page-link" href="{{backpack_url('dashboard') . '?date=' . $dUrl}}">
-                {{ $dTxt }}
-              </a></li>
-          @endfor
-
-          <li class="page-item active"><a class="page-link" href="#">
-            {{ $now->formatLocalized('%b %Y') }}
-          </a></li>
-
-          @for ($i = 1; $i < 3; $i++)
-            @php
-              $d = (clone $dC)->addMonths($i);
-              $dTxt = $d->formatLocalized('%b %Y');
-              $dUrl = $d->startOfMonth()->toDateString();
-            @endphp
-              <li class="page-item {{$i==2 ? 'hidden-xs' : ''}}"><a class="page-link" href="{{backpack_url('dashboard') . '?date=' . $dUrl}}">
-                {{ $dTxt }}
-              </a></li>
-          @endfor
-
-          <li class="page-item disabled hidden-xs"><a class="page-link" href="#">...</a></li>
-
-          @php
-            $d = (clone $dC)->addYear();
-            $dTxt = $d->formatLocalized('%b %Y');
-            $dUrl = $d->startOfMonth()->toDateString();
-          @endphp
-          <li class="page-item hidden-xs"><a class="page-link"
-            href="{{ backpack_url('dashboard') . '?date=' . $dUrl }}">
-            {{ $dTxt }} &raquo;
-          </a></li>
-
-        </ul>
-      </nav>
-    </div>
+  <div id="app">
+    <example-component></example-component>
   </div>
 
 @endsection
 
-@section('after_styles')
-<link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/bower_components/morris.js/morris.css">
+@section('before_styles')
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 <style>
-tr.clickable:hover{
-  cursor: pointer;
-  background-color: #ccc;
-}
 </style>
 
 @endsection
 
 @section('after_scripts')
-<script src="{{ asset('vendor/adminlte') }}/bower_components/raphael/raphael.min.js"></script>
-<script src="{{ asset('vendor/adminlte') }}/bower_components/morris.js/morris.min.js"></script>
-<script src="{{ asset('vendor/adminlte') }}/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 
 <script>
 $( document ).ready(function() {
